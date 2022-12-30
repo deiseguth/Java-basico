4e9279d01c717222889cf920a06de2c44c4a6271
deiseguth <deiiseguth@gmail.com>
2022-12-30 17:46:12 -0300

    aula 2 a 4

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //double salarioMinimo = 2500.32;
+        // A convenção americana é usar o ponto como vírgula.
+
+        int numero = 19;
+        System.out.print(numero);
+
+        //Para não mudar o valor de uma constante devo digitar final amtes da constante, e por convenção devo digitar o nome da constante em caixa alta
+        
     }
 }
