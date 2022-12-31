c8503de2419519f81e0db6704751c0e2ae32b0c9
deiseguth <deiiseguth@gmail.com>
2022-12-31 10:52:05 -0300

    1

---
+++ docs tipos e variaveis/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"TiposVariaveis","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"TiposVariaveis","l":"TiposVariaveis()","u":"%3Cinit%3E()"}];updateSearchResults();
